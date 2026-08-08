969061ac2d3ff90deb06dcf17dd3906b22916317
Abdelrahman-dev3 <abdoahmed2010201020@gmail.com>
2026-08-08 10:20:38 +0300

    Convert submodules to regular directories

--- Modules/Booking/Transformers/BookingDetailResource.php
+++ Modules/Booking/Transformers/BookingDetailResource.php
@@ -21,7 +21,7 @@ class BookingDetailResource extends JsonResource
     {
         $employee_id = optional($this->booking_service->first())->employee_id ?? optional($this->bookingPackages->first())->employee_id;
         $primaryAmount = ($this->booking_service ? $this->booking_service->sum('service_price') : 0) + ($this->bookingPackages ? $this->bookingPackages->sum('package_price') : 0);
-        $couponAmount = UserCouponRedeem::where('booking_id', $this->id)->sum('discount'); س
+        $couponAmount = UserCouponRedeem::where('booking_id', $this->id)->sum('discount'); 
         $couponcut_amount = $primaryAmount - $couponAmount;
         $tax_details = getBookingTaxamount(
             $primaryAmount + ($this->products ? $this->products->sum('discounted_price') : 0),
